ce52cbbfb072ce17b1300f9d5613c80503732136
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2021-02-12 13:14:02 +0200

    updated debugToFile logic

--- src/utils/debug-to-file.util.ts
+++ src/utils/debug-to-file.util.ts
@@ -2,12 +2,16 @@ import * as fs from 'fs';
 
 const folderPath = process.cwd() + '/debug';
 
+fs.rmdirSync(folderPath, { recursive: true });
+
 if (!fs.existsSync(folderPath)) {
     fs.mkdirSync(folderPath);
 }
 
-export function debugToFile(data, name = '') {
-    const string = JSON.stringify(data, null, '  ');
-    fs.writeFileSync(folderPath + '/' + name + string.slice(0, 10) + '.' + Date.now() + '.json', string);
-    return data;
+export function debugToFile(name = '') {
+    return (data) => {
+        const string = JSON.stringify(data, null, '  ');
+        fs.writeFileSync(folderPath + '/' + name + '.' + Date.now() + '.json', string);
+        return data;
+    };
 }
