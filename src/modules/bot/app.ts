b2bc4724e6e3e8ed7086ed795a92bd8e83576b9e
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2021-04-15 23:19:24 +0300

    prettied bot answer

--- src/modules/bot/app.ts
+++ src/modules/bot/app.ts
@@ -1,5 +1,6 @@
 import { Telegraf } from 'telegraf';
 import * as tg from 'telegraf/src/core/types/typegram';
+import * as tt from 'telegraf/src/telegram-types';
 
 import { youtubeService } from '../youtube-api';
 require('dotenv').config();
@@ -37,25 +38,46 @@ function getRandomIntArray(min, max, count) {
 
 function getRandomItemsFromArray(array, count) {
     const min = 0;
-    const max = array.length;
+    const max = array.length - 1;
     const randomIndexes = getRandomIntArray(min, max, count);
 
     return randomIndexes.map((index) => array[index]);
 }
 
+function delayMessage(time) {
+    return () => new Promise((resolve) => setTimeout(resolve, time));
+}
+
 bot.on('text', async (ctx) => {
-    ctx.reply(`Hello, @${ctx.update.message.from.username}!\nSearching for a passed channel - ${ctx.message.text}`);
+    const chatId = ctx.message.from.id;
+
+    ctx.reply(`👋 Hello, @${ctx.update.message.from.username}!\n🔍 Searching for a passed channel\n\n*${ctx.message.text}*`, { parse_mode: 'Markdown' });
+    ctx.telegram.sendChatAction(chatId, 'typing');
 
     try {
         const videos = await youtubeService.fetchVideosFromChannel(ctx.message.text, 30);
+        const channelData = {
+            thumbnail: videos.channel.thumbnail,
+            options: {
+                parse_mode: 'Markdown',
+                caption: '🔍 Found the channel!\n*'
+                    + videos.channel.title + '* with *' + videos.channel.videoCount + '* videos.\n\n'
+                    + '🎲 Showing random 10 videos:',
+            } as tt.ExtraPhoto,
+        };
 
-        const mediaGroup: ReadonlyArray<tg.InputMediaPhoto> = getRandomItemsFromArray(videos.videos, 10).map((video) => ({
+        const randomVideos = getRandomItemsFromArray(videos.videos, 10);
+        const mediaGroup: ReadonlyArray<tg.InputMediaPhoto> = randomVideos.map((video) => ({
             caption: video.title + '\n\n' + video.watchUrl,
             media: video.thumbnail,
             type: 'photo',
         }));
 
-        ctx.replyWithMediaGroup(mediaGroup);
+        ctx.telegram.sendPhoto(chatId, channelData.thumbnail, channelData.options)
+            .then(delayMessage(1000))
+            .then(() => ctx.telegram.sendDice(chatId))
+            .then(delayMessage(2000))
+            .then(() => ctx.replyWithMediaGroup(mediaGroup));
     } catch (e) {
         console.error(e);
         ctx.reply(`Cannot find ${ctx.message.text} channel. Please, try another`);
@@ -64,7 +86,7 @@ bot.on('text', async (ctx) => {
 
 });
 
-bot.launch();
+bot.launch().then(() => console.log('*** Bot has been started ***'));
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
